007dabb968e76eb2751771b3789cb4530a60edfc
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-06-14 12:35:27 +0530

    Update RestClientApplication.java

--- rest-client/src/main/java/com/example/restclient/RestClientApplication.java
+++ rest-client/src/main/java/com/example/restclient/RestClientApplication.java
@@ -8,4 +8,21 @@ public class RestClientApplication {
   SpringApplication.run(RestClientApplication.class,a);
   System.out.println("REST client invokes rest-order-service");
  }
+
+  @Bean
+  CommandLineRunner run(RestClient.Builder builder) {
+        return args -> {
+            RestClient client =
+                builder.baseUrl("http://rest-order-service:8080")
+                       .build();
+
+            String response =
+                client.get()
+                      .uri("/orders/101")
+                      .retrieve()
+                      .body(String.class);
+
+            System.out.println(response);
+        };
+    }
 }
