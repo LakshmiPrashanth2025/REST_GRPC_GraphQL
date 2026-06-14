5f450b18dc34bd1ac4fe256f95d1d2dec358717c
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-06-14 13:22:37 +0530

    Update GraphqlClientApplication.java

--- graphql-client/src/main/java/com/example/graphqlclient/GraphqlClientApplication.java
+++ graphql-client/src/main/java/com/example/graphqlclient/GraphqlClientApplication.java
@@ -5,10 +5,32 @@ import org.springframework.boot.autoconfigure.*;
 @SpringBootApplication
 public class GraphqlClientApplication {
  
- public static void main(String[] a) 
- {
-  SpringApplication.run(GraphqlClientApplication.class,a);
-  System.out.println("GraphQL client invokes graphql-order-service");
- }
+   public static void main(String[] a) 
+   {
+    SpringApplication.run(GraphqlClientApplication.class,a);
+    System.out.println("GraphQL client invokes graphql-order-service");
+   }
+
+    // 1. Define the Client Bean
+    @Bean
+    public HttpGraphQlClient graphQlClient() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:8080/graphql")
+                .build();
+        return HttpGraphQlClient.builder(webClient).build();
+    }
+
+    // 2. Execute the query using CommandLineRunner
+    @Bean
+    CommandLineRunner run(HttpGraphQlClient client) {
+        return args -> {
+            String document = "query { order }";
+
+            client.document(document)
+                    .retrieve("order")
+                    .toEntity(String.class)
+                    .subscribe(response -> System.out.println("Response from Server: " + response));
+        };
+    }
  
 }
