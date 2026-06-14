4ce086e0ff03425cf79a5e88191b2fddae1d8597
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-06-14 14:15:24 +0530

    Update UnifiedClientApplication.java

--- unified-api-client/src/main/java/com/example/unifiedapiclient/UnifiedClientApplication.java
+++ unified-api-client/src/main/java/com/example/unifiedapiclient/UnifiedClientApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class UnifiedClientApplication {
+ 
  public static void main(String[] a) {
   SpringApplication.run(UnifiedClientApplication.class,a);
-  System.out.println("Unified client invokes REST, gRPC and GraphQL services");
+  System.out.println("This is just a scaffolding of a Unified client invokes REST, gRPC and GraphQL services");
  }
+ 
 }
