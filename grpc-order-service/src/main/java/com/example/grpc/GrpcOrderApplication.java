ee8cb4bca6495c76406b9aa4ae745a1ee203d244
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-06-14 12:51:05 +0530

    Update GrpcOrderApplication.java

--- grpc-order-service/src/main/java/com/example/grpc/GrpcOrderApplication.java
+++ grpc-order-service/src/main/java/com/example/grpc/GrpcOrderApplication.java
@@ -1,7 +1,13 @@
 package com.example.grpc;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+
 @SpringBootApplication
 public class GrpcOrderApplication {
- public static void main(String[] a){SpringApplication.run(GrpcOrderApplication.class,a);}
+ 
+ public static void main(String[] a){
+  SpringApplication.run(GrpcOrderApplication.class,a);
+ }
+ 
 }
