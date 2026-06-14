32bc098675d3411ab4c22046c891e2cfb167bccb
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-06-14 13:41:51 +0530

    Update GrpcClientApplication.java

--- grpc-client/src/main/java/com/example/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpcclient/GrpcClientApplication.java
@@ -2,7 +2,9 @@ package com.example.grpcclient;
 
 import com.example.grpc.OrderRequest;
 import com.example.grpc.OrderServiceGrpc;
+
 import net.devh.boot.grpc.client.inject.GrpcClient;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
